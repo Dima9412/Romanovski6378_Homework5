9752ca86412aec18b7de6629b08ce794ee58df78
Dima9412 <165942648+Dima9412@users.noreply.github.com>
2024-09-19 18:16:47 +0300

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,18 +9,17 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        // Create instances of Teacher and Students
+        
         Teacher teacher = new Teacher("T1", "Konstantin Viktorovich");
         Student student1 = new Student("S1", "Dmitry");
         Student student2 = new Student("S2", "Maria");
 
-        // Create an instance of the controller
+        
         Controller controller = new Controller();
 
-        // Create a study group
+        
         StudyGroup studyGroup = controller.createStudyGroup(teacher, Arrays.asList(student1, student2));
 
-        // Print the created study group
+        
         System.out.println(studyGroup);
     }
-}
